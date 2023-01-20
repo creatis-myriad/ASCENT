82e6f54b0b01e2bb54fbcd903c0aa45e26fdf81f
Hang Jung Ling <hang-jung.ling@insa-lyon.fr>
2023-01-20 17:15:23 +0100

    Improve camus conversion script

--- ascent/dataset_conversion/camus.py
+++ ascent/dataset_conversion/camus.py
@@ -11,6 +11,8 @@ def convert_to_nnUNet(
     data_dir: Union[Path, str],
     output_dir: Union[Path, str],
     test: bool = False,
+    sequence: bool = False,
+    views: list = ["2CH", "4CH"],
 ) -> None:
     """Convert Camus dataset to nnUNet's format.
 
@@ -18,6 +20,8 @@ def convert_to_nnUNet(
         data_dir: Path to the dataset.
         output_dir: Path to the output folder to save the converted data.
         test: Whether is test dataset.
+        sequence: Whether to convert the whole sequence or 2CH/4CH ED/ES only. (images only)
+        views: Views to be converted.
     """
     if not test:
         images_out_dir = os.path.join(output_dir, "imagesTr")
@@ -26,13 +30,37 @@ def convert_to_nnUNet(
         images_out_dir = os.path.join(output_dir, "imagesTs")
         labels_out_dir = os.path.join(output_dir, "labelsTs")
 
+    os.makedirs(images_out_dir, exist_ok=True)
+    os.makedirs(labels_out_dir, exist_ok=True)
+
     for case in tqdm(os.listdir(data_dir)):
         case_path = os.path.join(data_dir, case)
         if os.listdir(case_path):
-            for view in ["2CH", "4CH"]:
-                for instant in ["ED", "ES"]:
-                    case_identifier = f"{case}_{view}_{instant}"
+            if not sequence:
+                for view in views:
+                    for instant in ["ED", "ES"]:
+                        case_identifier = f"{case}_{view}_{instant}"
+                        image = sitk.ReadImage(os.path.join(case_path, f"{case_identifier}.mhd"))
+                        if os.path.isfile(os.path.join(case_path, f"{case_identifier}_gt.mhd")):
+                            label = sitk.ReadImage(
+                                os.path.join(case_path, f"{case_identifier}_gt.mhd")
+                            )
+                        else:
+                            label = None
+                        sitk.WriteImage(
+                            image, os.path.join(images_out_dir, f"{case_identifier}_0000.nii.gz")
+                        )
+                        if label is not None:
+                            sitk.WriteImage(
+                                label, os.path.join(labels_out_dir, f"{case_identifier}.nii.gz")
+                            )
+            else:
+                for view in views:
+                    case_identifier = f"{case}_{view}_sequence"
                     image = sitk.ReadImage(os.path.join(case_path, f"{case_identifier}.mhd"))
+                    sitk.WriteImage(
+                        image, os.path.join(images_out_dir, f"{case_identifier}_0000.nii.gz")
+                    )
                     if os.path.isfile(os.path.join(case_path, f"{case_identifier}_gt.mhd")):
                         label = sitk.ReadImage(
                             os.path.join(case_path, f"{case_identifier}_gt.mhd")
@@ -46,6 +74,23 @@ def convert_to_nnUNet(
                         sitk.WriteImage(
                             label, os.path.join(labels_out_dir, f"{case_identifier}.nii.gz")
                         )
+
+
+def convert_to_CAMUS_submission(
+    predictions_dir: Union[Path, str], output_dir: Union[Path, str]
+) -> None:
+    """Convert predictions to correct format for submission.
+
+    Args:
+        predictions_dir: Path to the prediction folder.
+        output_dir: Path to the output folder to save the converted predictions.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+    for case in tqdm(os.listdir(predictions_dir)):
+        case_path = os.path.join(predictions_dir, case)
+        case_identifier = case[:-7]
+        image = sitk.ReadImage(case_path)
+        sitk.WriteImage(image, os.path.join(output_dir, f"{case_identifier}.mhd"))
 
 
 if __name__ == "__main__":
@@ -66,13 +111,23 @@ if __name__ == "__main__":
     os.makedirs(imagesTs, exist_ok=True)
     os.makedirs(labelsTs, exist_ok=True)
 
+    # Convert train data to nnUNet's format
     convert_to_nnUNet(base, output_dir)
-    # generate_dataset_json(
-    #     os.path.join(output_dir, "dataset.json"),
-    #     imagesTr,
-    #     imagesTs,
-    #     ("US",),
-    #     {0: "background", 1: "LV", 2: "MYO", 3: "LA"},
-    #     dataset_name,
-    # )
+
+    # Generate dataset.json
+    generate_dataset_json(
+        os.path.join(output_dir, "dataset.json"),
+        imagesTr,
+        imagesTs,
+        ("US",),
+        {0: "background", 1: "LV", 2: "MYO", 3: "LA"},
+        dataset_name,
+    )
+
+    # Convert test data to nnUNet's format
     convert_to_nnUNet(test_data, output_dir, test=True)
+
+    # Convert predictions in Nifti format to raw/mhd
+    prediction_dir = "C:/Users/ling/Desktop/camus_test/inference_raw"
+    submission_dir = "C:/Users/ling/Desktop/camus_test/submission"
+    convert_to_CAMUS_submission(prediction_dir, submission_dir)
