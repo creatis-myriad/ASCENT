f2bfbe4c68c0b3300644d5e2bfe14fad1dd5cae3
Hang Jung Ling <hang-jung.ling@insa-lyon.fr>
2023-01-23 16:34:38 +0100

    Update conversion script for TED256

--- ascent/dataset_conversion/ted.py
+++ ascent/dataset_conversion/ted.py
@@ -2,9 +2,12 @@ import os
 from pathlib import Path
 from typing import Union
 
+import numpy as np
 import SimpleITK as sitk
 from tqdm import tqdm
 from utils import generate_dataset_json
+
+from ascent.preprocessing.preprocessing import resample_image, resample_label
 
 
 def convert_to_nnUNet(
@@ -13,6 +16,7 @@ def convert_to_nnUNet(
     test: bool = False,
     sequence: bool = False,
     views: list = ["2CH", "4CH"],
+    resize: bool = False,
 ) -> None:
     """Convert Camus dataset to nnUNet's format.
 
@@ -22,6 +26,7 @@ def convert_to_nnUNet(
         test: Whether is test dataset.
         sequence: Whether to convert the whole sequence or 2CH/4CH ED/ES only. (images only)
         views: Views to be converted.
+        resize: Whether to resize images to 256x256xT.
     """
     if not test:
         images_out_dir = os.path.join(output_dir, "imagesTr")
@@ -53,10 +58,34 @@ def convert_to_nnUNet(
                             )
                         else:
                             label = None
+
+                        if resize:
+                            ori_shape = image.GetSize()
+                            ori_spacing = image.GetSpacing()
+                            new_shape = [256, 256, ori_shape[-1]]
+                            new_spacing = (
+                                np.array(ori_spacing) * np.array(ori_shape) / np.array(new_shape)
+                            )
+                            image_array = sitk.GetArrayFromImage(image).transpose(2, 1, 0)
+                            image_array = image_array[None]
+                            resized_image_array = resample_image(image_array, new_shape, True)
+                            image = sitk.GetImageFromArray(
+                                resized_image_array[0].transpose(2, 1, 0).astype(np.uint8)
+                            )
+                            image.SetSpacing(new_spacing)
+
                         sitk.WriteImage(
                             image, os.path.join(images_out_dir, f"{case_identifier}_0000.nii.gz")
                         )
                         if label is not None:
+                            if resize:
+                                label_array = sitk.GetArrayFromImage(label).transpose(2, 1, 0)
+                                label_array = label_array[None]
+                                resized_label_array = resample_label(label_array, new_shape, True)
+                                label = sitk.GetImageFromArray(
+                                    resized_label_array[0].transpose(2, 1, 0)
+                                )
+                                label.SetSpacing(new_spacing)
                             sitk.WriteImage(
                                 label, os.path.join(labels_out_dir, f"{case_identifier}.nii.gz")
                             )
@@ -73,10 +102,32 @@ def convert_to_nnUNet(
                         )
                     else:
                         label = None
+
+                    if resize:
+                        ori_shape = image.GetSize()
+                        ori_spacing = image.GetSpacing()
+                        new_shape = [256, 256, ori_shape[-1]]
+                        new_spacing = (
+                            np.array(ori_spacing) * np.array(ori_shape) / np.array(new_shape)
+                        )
+                        image_array = sitk.GetArrayFromImage(image).transpose(2, 1, 0)
+                        image_array = image_array[None]
+                        resized_image_array = resample_image(image_array, new_shape, True)
+                        image = sitk.GetImageFromArray(resized_image_array[0].transpose(2, 1, 0))
+                        image.SetSpacing(new_spacing)
+
                     sitk.WriteImage(
                         image, os.path.join(images_out_dir, f"{case_identifier}_0000.nii.gz")
                     )
                     if label is not None:
+                        if resize:
+                            label_array = sitk.GetArrayFromImage(label).transpose(2, 1, 0)
+                            label_array = label_array[None]
+                            resized_label_array = resample_label(label_array, new_shape, True)
+                            label = sitk.GetImageFromArray(
+                                resized_label_array[0].transpose(2, 1, 0).astype(np.uint8)
+                            )
+                            label.SetSpacing(new_spacing)
                         sitk.WriteImage(
                             label, os.path.join(labels_out_dir, f"{case_identifier}.nii.gz")
                         )
@@ -96,15 +147,40 @@ def convert_to_CAMUS_submission(
         case_path = os.path.join(predictions_dir, case)
         case_identifier = case[:-7]
         image = sitk.ReadImage(case_path)
-        sitk.WriteImage(image, os.path.join(output_dir, f"{case_identifier}.mhd"))
+        if "sequence" in case_identifier:
+            case_identifier = case_identifier[:-9]
+            image_array = sitk.GetArrayFromImage(image)
+            spacing = image.GetSpacing()
+            ori_spacing = [0.308, 0.154, 1.54]
+            if not np.all(np.array(spacing) == np.array(ori_spacing)):
+                shape = image.GetSize()
+                image_array = image_array.transpose(2, 1, 0)
+                image_array = image_array[None]
+                ori_shape = (
+                    (np.array(spacing) * np.array(shape) / np.array(ori_spacing))
+                    .astype(np.int8)
+                    .tolist()
+                )
+                resized = resample_label(image_array, ori_shape, True)
+                image_array = resized[0]
+                spacing = ori_spacing
+
+            ed_frame = sitk.GetImageFromArray(image_array[0:1].astype(np.uint8))
+            es_frame = sitk.GetImageFromArray(image_array[-2:-1].astype(np.uint8))
+            ed_frame.SetSpacing(spacing)
+            es_frame.SetSpacing(spacing)
+            sitk.WriteImage(ed_frame, os.path.join(output_dir, f"{case_identifier}_ED.mhd"))
+            sitk.WriteImage(es_frame, os.path.join(output_dir, f"{case_identifier}_ES.mhd"))
+        else:
+            sitk.WriteImage(image, os.path.join(output_dir, f"{case_identifier}.mhd"))
 
 
 if __name__ == "__main__":
     base = "C:/Users/ling/Desktop/Camus/4CH_full_cycle"
-    output_dir = "C:/Users/ling/Desktop/Thesis/REPO/ASCENT/data/TED/raw"
+    output_dir = "C:/Users/ling/Desktop/Thesis/REPO/ASCENT/data/TED256/raw"
     os.makedirs(output_dir, exist_ok=True)
 
-    dataset_name = "TED"
+    dataset_name = "TED256"
 
     imagesTr = os.path.join(output_dir, "imagesTr")
     labelsTr = os.path.join(output_dir, "labelsTr")
@@ -117,7 +193,7 @@ if __name__ == "__main__":
     os.makedirs(labelsTs, exist_ok=True)
 
     # Convert train data to nnUNet's format
-    convert_to_nnUNet(base, output_dir, sequence=True, views=["4CH"])
+    convert_to_nnUNet(base, output_dir, sequence=True, views=["4CH"], resize=True)
 
     # Generate dataset.json
     generate_dataset_json(
@@ -129,7 +205,7 @@ if __name__ == "__main__":
         dataset_name,
     )
 
-    # Convert predictions in Nifti format to raw/mhd
-    prediction_dir = "C:/Users/ling/Desktop/camus_test/inference_raw"
-    submission_dir = "C:/Users/ling/Desktop/camus_test/submission"
-    convert_to_CAMUS_submission(prediction_dir, submission_dir)
+    # # Convert predictions in Nifti format to raw/mhd
+    # prediction_dir = "C:/Users/ling/Desktop/camus_sequence_test/inference_raw"
+    # submission_dir = "C:/Users/ling/Desktop/camus_sequence_test/submission"
+    # convert_to_CAMUS_submission(prediction_dir, submission_dir)
