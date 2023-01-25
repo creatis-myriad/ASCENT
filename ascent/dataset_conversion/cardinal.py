6ac9bfdbc12952ad518773e44ca35d676ea23e4e
Hang Jung Ling <hang-jung.ling@insa-lyon.fr>
2023-01-25 10:13:17 +0100

    Add cardinal conversion script and cardinal2 configs

---
+++ ascent/dataset_conversion/cardinal.py
@@ -0,0 +1,188 @@
+import os
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+import SimpleITK as sitk
+from tqdm import tqdm
+from utils import generate_dataset_json
+
+from ascent.preprocessing.preprocessing import resample_image, resample_label
+
+
+def convert_to_nnUNet(
+    data_dir: Union[Path, str],
+    output_dir: Union[Path, str],
+    test: bool = False,
+    views: list = ["A2C", "A4C"],
+    resize: bool = False,
+    txt_file: Union[Path, str] = None,
+) -> None:
+    """Convert Cardinal dataset to nnUNet's format.
+
+    Args:
+        data_dir: Path to the dataset.
+        output_dir: Path to the output folder to save the converted data.
+        test: Whether is test dataset.
+        views: Views to be converted.
+        resize: Whether to resize images to 256x256xT.
+        txt_file: Text file that contains patient indexes to select.
+    """
+    if not test:
+        images_out_dir = os.path.join(output_dir, "imagesTr")
+        labels_out_dir = os.path.join(output_dir, "labelsTr")
+    else:
+        images_out_dir = os.path.join(output_dir, "imagesTs")
+        labels_out_dir = os.path.join(output_dir, "labelsTs")
+
+    os.makedirs(images_out_dir, exist_ok=True)
+    os.makedirs(labels_out_dir, exist_ok=True)
+
+    all_cases = [
+        file[:4]
+        for file in os.listdir(data_dir)
+        if os.path.isfile(os.path.join(data_dir, file)) and file.endswith(".nii.gz")
+    ]
+    all_cases = np.unique(np.array(all_cases)).tolist()
+
+    if txt_file is not None:
+        if os.path.isfile(txt_file):
+            with open(txt_file) as f:
+                selected_cases = f.read().splitlines()
+    else:
+        selected_cases = [
+            file[:4]
+            for file in os.listdir(data_dir)
+            if os.path.isfile(os.path.join(data_dir, file)) and file.endswith(".nii.gz")
+        ]
+        selected_cases = np.unique(np.array(selected_cases)).tolist()
+
+    for case in tqdm(selected_cases):
+        for view in views:
+            case_identifier = f"{case}_{view}"
+            image = sitk.ReadImage(os.path.join(data_dir, f"{case_identifier}_bmode.nii.gz"))
+            image.SetSpacing([*image.GetSpacing()[:-1], 1.54])
+            if os.path.isfile(os.path.join(data_dir, f"{case_identifier}_mask.nii.gz")):
+                label = sitk.ReadImage(os.path.join(data_dir, f"{case_identifier}_mask.nii.gz"))
+                label.SetSpacing([*label.GetSpacing()[:-1], 1.54])
+            else:
+                label = None
+
+            if resize:
+                ori_shape = image.GetSize()
+                ori_spacing = image.GetSpacing()
+                new_shape = [256, 256, ori_shape[-1]]
+                new_spacing = np.array(ori_spacing) * np.array(ori_shape) / np.array(new_shape)
+                image_array = sitk.GetArrayFromImage(image).transpose(2, 1, 0)
+                image_array = image_array[None]
+                resized_image_array = resample_image(image_array, new_shape, True)
+                image = sitk.GetImageFromArray(
+                    resized_image_array[0].transpose(2, 1, 0).astype(np.uint8)
+                )
+                image.SetSpacing(new_spacing)
+
+            sitk.WriteImage(image, os.path.join(images_out_dir, f"{case_identifier}_0000.nii.gz"))
+            if label is not None:
+                if resize:
+                    label_array = sitk.GetArrayFromImage(label).transpose(2, 1, 0)
+                    label_array = label_array[None]
+                    resized_label_array = resample_label(label_array, new_shape, True)
+                    label = sitk.GetImageFromArray(resized_label_array[0].transpose(2, 1, 0))
+                    label.SetSpacing(new_spacing)
+                sitk.WriteImage(label, os.path.join(labels_out_dir, f"{case_identifier}.nii.gz"))
+
+
+def convert_to_CAMUS_submission(
+    predictions_dir: Union[Path, str],
+    output_dir: Union[Path, str],
+    la_predictions_dir: Union[Path, str],
+) -> None:
+    """Convert predictions to correct format for submission.
+
+    Args:
+        predictions_dir: Path to the prediction folder.
+        output_dir: Path to the output folder to save the converted predictions.
+        la_predictions_dir: Path to 2D nnUNet's predictions to retrieve the left atrium segmentations.
+    """
+    os.makedirs(output_dir, exist_ok=True)
+    for case in tqdm(os.listdir(predictions_dir)):
+        case_path = os.path.join(predictions_dir, case)
+        case_identifier = case[:-7]
+        image = sitk.ReadImage(case_path)
+        if "sequence" in case_identifier:
+            case_identifier = case_identifier[:-9]
+            image_array = sitk.GetArrayFromImage(image)
+            spacing = image.GetSpacing()
+            ori_spacing = [0.308, 0.154, 1.54]
+            if not np.all(np.array(np.round(spacing, 3))[:-1] == np.array(ori_spacing)[:-1]):
+                shape = image.GetSize()
+                image_array = image_array.transpose(2, 1, 0)
+                image_array = image_array[None]
+                ori_shape = (
+                    (np.array(spacing) * np.array(shape) / np.array(ori_spacing))
+                    .astype(np.int8)
+                    .tolist()
+                )
+                resized = resample_label(image_array, ori_shape, True)
+                image_array = resized[0]
+            spacing = ori_spacing
+
+            la_ed_frame = sitk.GetArrayFromImage(
+                sitk.ReadImage(os.path.join(la_predictions_dir, f"{case_identifier}_ED.mhd"))
+            )
+            la_es_frame = sitk.GetArrayFromImage(
+                sitk.ReadImage(os.path.join(la_predictions_dir, f"{case_identifier}_ES.mhd"))
+            )
+
+            ed_frame_array = image_array[0:1].astype(np.uint8)
+            es_frame_array = image_array[-1:].astype(np.uint8)
+
+            ed_frame_array[np.logical_and(ed_frame_array == 0, la_ed_frame == 3)] = 3
+            es_frame_array[np.logical_and(es_frame_array == 0, la_es_frame == 3)] = 3
+
+            ed_frame = sitk.GetImageFromArray(ed_frame_array)
+            es_frame = sitk.GetImageFromArray(es_frame_array)
+            ed_frame.SetSpacing(spacing)
+            es_frame.SetSpacing(spacing)
+            sitk.WriteImage(ed_frame, os.path.join(output_dir, f"{case_identifier}_ED.mhd"))
+            sitk.WriteImage(es_frame, os.path.join(output_dir, f"{case_identifier}_ES.mhd"))
+        else:
+            sitk.WriteImage(image, os.path.join(output_dir, f"{case_identifier}.mhd"))
+
+
+if __name__ == "__main__":
+    base = "C:/Users/ling/Desktop/Dataset/Bmode/Cardinal"
+    output_dir = "C:/Users/ling/Desktop/Thesis/REPO/ASCENT/data/CARDINAL2/raw"
+    txt_file = "C:/Users/ling/Desktop/Dataset/Bmode/Cardinal/A2C_100_patients.txt"
+    os.makedirs(output_dir, exist_ok=True)
+
+    dataset_name = "TED + Cardinal_100_A2C"
+
+    imagesTr = os.path.join(output_dir, "imagesTr")
+    labelsTr = os.path.join(output_dir, "labelsTr")
+    imagesTs = os.path.join(output_dir, "imagesTs")
+    labelsTs = os.path.join(output_dir, "labelsTs")
+
+    os.makedirs(imagesTr, exist_ok=True)
+    os.makedirs(labelsTr, exist_ok=True)
+    os.makedirs(imagesTs, exist_ok=True)
+    os.makedirs(labelsTs, exist_ok=True)
+
+    # Convert train data to nnUNet's format
+    convert_to_nnUNet(base, output_dir, views=["A2C"], resize=False, txt_file=txt_file)
+
+    # Generate dataset.json
+    generate_dataset_json(
+        os.path.join(output_dir, "dataset.json"),
+        imagesTr,
+        imagesTs,
+        ("US",),
+        {0: "background", 1: "LV", 2: "MYO"},
+        dataset_name,
+    )
+
+    # Convert predictions in Nifti format to raw/mhd
+    prediction_dir = "C:/Users/ling/Desktop/camus_sequence_test/nnUNet_3D_processed"
+    submission_dir = "C:/Users/ling/Desktop/camus_sequence_test/submission_cardinal"
+    la_predictions_dir = "C:/Users/ling/Desktop/camus_test/submission"
+    convert_to_CAMUS_submission(prediction_dir, submission_dir, la_predictions_dir)
