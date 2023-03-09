18ee82bba91b8f276ec36b7bf596d967cece0aea
Hang Jung Ling <106228386+HangJung97@users.noreply.github.com>
2023-03-09 11:41:57 +0100

    Created abstract classes for `train`/`evaluate`/`predict` + Forced tags interpolation when printing (#73)
    Created abstract classes for train/evaluate/predict + Tags interpolation

--- ascent/train.py
+++ ascent/train.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from typing import List, Optional, Tuple
 
 import hydra
@@ -31,116 +32,125 @@ from ascent import utils
 log = utils.get_pylogger(__name__)
 
 
-@utils.task_wrapper
-def train(cfg: DictConfig) -> Tuple[dict, dict]:
-    """Trains the model. Can additionally evaluate on a testset, using best weights obtained during
-    training.
+class AscentTrainer(ABC):
+    """Abstract trainer that runs the main training loop using Lightning Trainer."""
 
-    This method is wrapped in @task_wrapper decorator which applies extra utilities
-    before and after the call.
+    @staticmethod
+    @utils.task_wrapper
+    def train(cfg: DictConfig) -> Tuple[dict, dict]:
+        """Trains the model. Can additionally evaluate on a testset, using best weights obtained
+        during training.
 
-    Args:
-        cfg (DictConfig): Configuration composed by Hydra.
+        This method is wrapped in @task_wrapper decorator which applies extra utilities
+        before and after the call.
 
-    Returns:
-        Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
-    """
+        Args:
+            cfg (DictConfig): Configuration composed by Hydra.
 
-    # set seed for random number generators in pytorch, numpy and python.random
-    if cfg.get("seed"):
-        pl.seed_everything(cfg.seed, workers=True)
+        Returns:
+            Tuple[dict, dict]: Dict with metrics and dict with all instantiated objects.
+        """
 
-    log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
-    datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
+        # set seed for random number generators in pytorch, numpy and python.random
+        if cfg.get("seed"):
+            pl.seed_everything(cfg.seed, workers=True)
 
-    log.info(f"Instantiating model <{cfg.model._target_}>")
-    model: LightningModule = hydra.utils.instantiate(cfg.model)
+        log.info(f"Instantiating datamodule <{cfg.datamodule._target_}>")
+        datamodule: LightningDataModule = hydra.utils.instantiate(cfg.datamodule)
 
-    if cfg.get("transfer_training") and cfg.get("ckpt_path"):
-        log.info(f"Loading weights from {cfg.ckpt_path}")
-        model.load_state_dict(
-            torch.load(cfg.get("ckpt_path"), map_location=model.device)["state_dict"]
+        log.info(f"Instantiating model <{cfg.model._target_}>")
+        model: LightningModule = hydra.utils.instantiate(cfg.model)
+
+        if cfg.get("transfer_training") and cfg.get("ckpt_path"):
+            log.info(f"Loading weights from {cfg.ckpt_path}")
+            model.load_state_dict(
+                torch.load(cfg.get("ckpt_path"), map_location=model.device)["state_dict"]
+            )
+
+        log.info("Instantiating callbacks...")
+        callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
+
+        log.info("Instantiating loggers...")
+        logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
+
+        log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
+        trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
+
+        object_dict = {
+            "cfg": cfg,
+            "datamodule": datamodule,
+            "model": model,
+            "callbacks": callbacks,
+            "logger": logger,
+            "trainer": trainer,
+        }
+
+        if logger:
+            log.info("Logging hyperparameters!")
+            utils.log_hyperparameters(object_dict)
+
+        if cfg.get("train"):
+            log.info("Starting training!")
+            if not cfg.transfer_training:
+                trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+            else:
+                trainer.fit(model=model, datamodule=datamodule)
+
+            if isinstance(trainer.logger, CometLogger) and cfg.comet_save_model:
+                if cfg.get("best_model"):
+                    trainer.logger.experiment.log_model(
+                        "model", trainer.checkpoint_callback.best_model_path
+                    )
+                else:
+                    trainer.logger.experiment.log_model(
+                        "model", trainer.checkpoint_callback.last_model_path
+                    )
+
+        train_metrics = trainer.callback_metrics
+
+        if cfg.get("test"):
+            log.info("Starting testing!")
+            if cfg.get("best_model"):
+                ckpt_path = trainer.checkpoint_callback.best_model_path
+                if ckpt_path == "":
+                    log.warning("Best ckpt not found! Using current weights for testing...")
+                    ckpt_path = None
+                else:
+                    log.info(f"Loading best ckpt: {ckpt_path}")
+            else:
+                ckpt_path = trainer.checkpoint_callback.last_model_path
+                if ckpt_path == "":
+                    log.warning("Last ckpt not found! Using current weights for testing...")
+                    ckpt_path = None
+                else:
+                    log.info(f"Loading last ckpt: {ckpt_path}")
+            trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
+
+        test_metrics = trainer.callback_metrics
+
+        # merge train and test metrics
+        metric_dict = {**train_metrics, **test_metrics}
+
+        return metric_dict, object_dict
+
+    @staticmethod
+    @hydra.main(version_base="1.3.2", config_path="../configs", config_name="train")
+    def main(cfg: DictConfig) -> Optional[float]:
+        # train the model
+        metric_dict, _ = AscentTrainer.train(cfg)
+
+        # safely retrieve metric value for hydra-based hyperparameter optimization
+        metric_value = utils.get_metric_value(
+            metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
         )
 
-    log.info("Instantiating callbacks...")
-    callbacks: List[Callback] = utils.instantiate_callbacks(cfg.get("callbacks"))
-
-    log.info("Instantiating loggers...")
-    logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
-
-    log.info(f"Instantiating trainer <{cfg.trainer._target_}>")
-    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
-
-    object_dict = {
-        "cfg": cfg,
-        "datamodule": datamodule,
-        "model": model,
-        "callbacks": callbacks,
-        "logger": logger,
-        "trainer": trainer,
-    }
-
-    if logger:
-        log.info("Logging hyperparameters!")
-        utils.log_hyperparameters(object_dict)
-
-    if cfg.get("train"):
-        log.info("Starting training!")
-        if not cfg.transfer_training:
-            trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
-        else:
-            trainer.fit(model=model, datamodule=datamodule)
-
-        if isinstance(trainer.logger, CometLogger) and cfg.comet_save_model:
-            if cfg.get("best_model"):
-                trainer.logger.experiment.log_model(
-                    "model", trainer.checkpoint_callback.best_model_path
-                )
-            else:
-                trainer.logger.experiment.log_model(
-                    "model", trainer.checkpoint_callback.last_model_path
-                )
-
-    train_metrics = trainer.callback_metrics
-
-    if cfg.get("test"):
-        log.info("Starting testing!")
-        if cfg.get("best_model"):
-            ckpt_path = trainer.checkpoint_callback.best_model_path
-            if ckpt_path == "":
-                log.warning("Best ckpt not found! Using current weights for testing...")
-                ckpt_path = None
-            else:
-                log.info(f"Loading best ckpt: {ckpt_path}")
-        else:
-            ckpt_path = trainer.checkpoint_callback.last_model_path
-            if ckpt_path == "":
-                log.warning("Last ckpt not found! Using current weights for testing...")
-                ckpt_path = None
-            else:
-                log.info(f"Loading last ckpt: {ckpt_path}")
-        trainer.test(model=model, datamodule=datamodule, ckpt_path=ckpt_path)
-
-    test_metrics = trainer.callback_metrics
-
-    # merge train and test metrics
-    metric_dict = {**train_metrics, **test_metrics}
-
-    return metric_dict, object_dict
+        # return optimized metric
+        return metric_value
 
 
-@hydra.main(version_base="1.3.2", config_path="../configs", config_name="train.yaml")
-def main(cfg: DictConfig) -> Optional[float]:
-    # train the model
-    metric_dict, _ = train(cfg)
-
-    # safely retrieve metric value for hydra-based hyperparameter optimization
-    metric_value = utils.get_metric_value(
-        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
-    )
-
-    # return optimized metric
-    return metric_value
+def main():
+    """Run the script."""
+    AscentTrainer.main()
 
 
 if __name__ == "__main__":
