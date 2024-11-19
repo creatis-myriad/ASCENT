cd8f205bbe54953f03c0eab25592709b474b4563
Hang Jung Ling <hang-jung.ling@insa-lyon.fr>
2024-11-19 10:55:17 +0100

    Test new iterable

--- ascent/utils/dataset.py
+++ ascent/utils/dataset.py
@@ -1,21 +1,5 @@
 import random
 
-# class nnUNet_Iterator:
-#     """Infinite iterable dataset for nnUNet's pipeline,"""
-#
-#     def __init__(self, list_of_dicts: list[dict[str, str]]):
-#         """Initialize class instance.
-#
-#         Args:
-#             list of dict: List of dictionaries containing the path to the preprocessed .npy data.
-#         """
-#         self.list_of_dicts = list_of_dicts
-#
-#     def __iter__(self):  # noqa: D102
-#         while True:
-#             # draw a random dictionary from the list
-#             yield random.choice(self.list_of_dicts)  # nosec B311
-
 
 class nnUNet_Iterator:
     """Infinite iterable dataset for nnUNet's pipeline,"""
@@ -29,9 +13,26 @@ class nnUNet_Iterator:
         self.list_of_dicts = list_of_dicts
 
     def __iter__(self):  # noqa: D102
-        return self
+        while True:
+            # draw a random dictionary from the list
+            yield random.choice(self.list_of_dicts)  # nosec B311
 
-    def __next__(self):  # noqa: D102
-        # draw a random dictionary from the list
-        random_dict = random.choice(self.list_of_dicts)  # nosec B311
-        return random_dict
+
+# class nnUNet_Iterator:
+#     """Infinite iterable dataset for nnUNet's pipeline,"""
+#
+#     def __init__(self, list_of_dicts: list[dict[str, str]]):
+#         """Initialize class instance.
+#
+#         Args:
+#             list of dict: List of dictionaries containing the path to the preprocessed .npy data.
+#         """
+#         self.list_of_dicts = list_of_dicts
+#
+#     def __iter__(self):  # noqa: D102
+#         return self
+#
+#     def __next__(self):  # noqa: D102
+#         # draw a random dictionary from the list
+#         random_dict = random.choice(self.list_of_dicts)  # nosec B311
+#         return random_dict
